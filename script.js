83f03b5567d066a7ae433f385be7dafe2692e042
calexd <caledarvs@gmail.com>
2024-03-09 23:21:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let attribute = document.getElementById('imgBackG')
+let changeButton = document.getElementById('changeButton')
+
+changeButton.addEventListener('click', () =>{
+    attribute.setAttribute('src', 'https://images2.alphacoders.com/132/1329016.png')
+})
+
+
